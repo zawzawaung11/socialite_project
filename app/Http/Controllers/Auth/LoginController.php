6fa91b4703165f23336c52546c18694a63d7da3d
zawzawaung <newt11@gmail.com>
2020-07-16 16:17:07 +0630

    Initial commit

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+use Hash;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+	public function redirectToProvider()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+	public function handleProviderCallback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+			
+		$user = User::where('provider_id', $githubUser->id)->first();
+        if (!$user) {
+            // add user to database
+            $user = User::create([
+                'email' => $githubUser->email,
+                'name' => $githubUser->nickname,
+                'provider_id' => $githubUser->id,
+				'password' => Hash::make('yourpassword'),
+            ]);
+        }
+
+        // login the user
+        Auth::login($user, true);
+
+        return redirect($this->redirectTo);
+
+		
+        // $user->token;
+    }	
+	 
+	 
+    public function __construct()
+    {
+        $this->middleware('guest', ['except' => 'logout']);
+    }
+}
